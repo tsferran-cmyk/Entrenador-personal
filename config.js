d7d35ef1f035d2f053c3727b5daf2466d7aec803
Ferran Trigo <el_teu_email@exemple.comts.ferran@gmail.com>
2026-09-15 09:36:55 +0200

    Update Google OAuth client ID

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.GOOGLE_CLIENT_ID = '565032537440-1j4tqj02d3t5j4f9kh9loancj7nrm9e.apps.googleusercontent.com';
+window.GOOGLE_CLIENT_ID = '565032537440-l9t4jq02j3t5j4fs9kh9loancj7nrm9e.apps.googleusercontent.com';
 window.GOOGLE_API_KEY = 'YOUR_GOOGLE_API_KEY';
